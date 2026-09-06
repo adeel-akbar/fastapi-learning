9f0482c8c57f4ac9ac46bda7e4d80042060e00a3
Adeel Akbar <theadeel.dev@gmail.com>
2026-09-06 22:02:37 +0500

    Add CRUD operations using SQLAlchemy 2.x

--- 05-sqlalchemy-and-orm/latest-sqlalchemy/database.py
+++ 05-sqlalchemy-and-orm/latest-sqlalchemy/database.py
@@ -16,3 +16,9 @@ SessionLocal = sessionmaker(
 class Base(DeclarativeBase):
     pass
 
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
---
+++ 05-sqlalchemy-and-orm/latest-sqlalchemy/schemas.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostResponse(PostCreate):
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes = True)
