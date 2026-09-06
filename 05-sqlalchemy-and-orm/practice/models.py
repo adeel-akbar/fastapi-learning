8a57e814b30fd657986aaff03e32d0768819c712
Adeel Akbar <theadeel.dev@gmail.com>
2026-09-06 16:11:41 +0500

    Add practice using SQLAlchemy 2.x

---
+++ 05-sqlalchemy-and-orm/practice/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id: Mapped[int] = mapped_column(primary_key= True, nullable= False)
+    title: Mapped[str] = mapped_column(nullable= False)
+    content: Mapped[str] = mapped_column(nullable= False)
+    published: Mapped[bool] = mapped_column(nullable= False, server_default= "true")
+    created_at: Mapped[datetime]= mapped_column(TIMESTAMP(timezone= True),
+                server_default= func.now(), nullable= False)
