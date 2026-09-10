1e6f8573e2d72fb395778380be814308de32cfb9
Adeel Akbar <theadeel.dev@gmail.com>
2026-09-10 16:28:39 +0500

    Add JWT login and token verification

--- 06-users-and-authentication/app/utilis.py
+++ 06-users-and-authentication/app/utilis.py
@@ -3,3 +3,6 @@ from pwdlib import PasswordHash
 password_hasher = PasswordHash.recommended()
 def hash_password(password: str):
     return password_hasher.hash(password)
+
+def verify(plain, hash):
+    return password_hasher.verify(plain, hash)
