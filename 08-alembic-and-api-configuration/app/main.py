8d6a5795282e2f977b5a9c379cbe46f53969c346
Adeel Akbar <theadeel.dev@gmail.com>
2026-09-15 23:57:20 +0500

    Remove Base.metadata.create_all in favor of Alembic migrations

--- 08-alembic-and-api-configuration/app/main.py
+++ 08-alembic-and-api-configuration/app/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 
-from . import models
-from .database import engine
 from .routers import auth, posts, users, votes
 
 app = FastAPI()
-models.Base.metadata.create_all(bind= engine)
 
 app.include_router(posts.router)
 app.include_router(users.router)
